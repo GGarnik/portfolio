d3582a64f2b75c3b10133f351cf068e95f0bb3ce
GGarnik <garnik2828@gmail.com>
2024-08-07 03:26:54 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="manrope-font header fixed"></header>
+      <p className="manrope-font title animation-title-1">Эй, тут так пусто...</p>
+
+    </div>
+  );
+}
+
+
+export default App;
