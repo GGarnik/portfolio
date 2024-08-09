b8b76de538a95d0ca95ff1aec4960e6b60d1f5eb
GGarnik <garnik2828@gmail.com>
2024-08-09 03:00:28 +0200

    Commit 1 - Added some containers
    DONE:
    - Smooth animation of text appearance
    - Changing card parameters on mouse hover
    
    TO-DO:
    - Configure correct display of containers
    - Add active elements to the header
    - Add a redirect to the project page when clicking on the project card

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ function App() {
     <div className="App">
       <header className="manrope-font header fixed"></header>
       <p className="manrope-font title animation-title-1">Эй, тут так пусто...</p>
-
+      <div className='container animation-title-1'>
+        <div className='container2'>
+          <img src="drill.png" alt="the drill" className='flex project-image'></img>
+          <h1 className="flex manrope-font2 projects-text">Это один из моих проектов.</h1>
+        </div>
+        <div className='container2'>
+          <img src="drill.png" alt="the drill" className='flex project-image'></img>
+          <h1 className="flex manrope-font2 projects-text">Это ещё один из моих проектов.</h1>
+        </div>
+        <div className='container2'>
+          <img src="drill.png" alt="the drill" className='flex project-image'></img>
+          <h1 className="flex manrope-font2 projects-text">Чёт дохуя у меня проектов.</h1>
+        </div>
+      </div>
     </div>
   );
 }
